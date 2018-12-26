93dc4969280f8649af6a0e32ccee69e0030df9f0
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-26 23:43:44 +0000

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -1,15 +1,13 @@
 const Discord = module.require("discord.js");
 const client = new Discord.Client();
-const mentionHook = new Discord.WebhookClient("526376828290596865", "xR-_drIyJJKyEv11nH8pUgfkm5EndIY3wdMD77lVEgpMW2nRc9qofei23BkqEriF6gOy");
 
 module.exports.run = async (client, message, args) => {
-	//!addrole @andrew Dog Person
   if (message.author.id !== '501649887411175435') return message.reply("Sorry pal, you can't do that.");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("Couldn't find that user, yo.");
   let role = args[1];
   if(!role) return message.reply("Specify a role!");
-  let gRole = message.guild.roles.find(`id`, role);
+  let gRole = message.guild.roles.find(`name`, role);
   if(!gRole) return message.reply("Couldn't find that role.");
 
   if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
@@ -23,7 +21,5 @@ module.exports.run = async (client, message, args) => {
 }
 
 module.exports.help = {
-    name: "createserver",
-    description: "Announce message",
-    usage: "createserver"
+    name: "role"
 }
