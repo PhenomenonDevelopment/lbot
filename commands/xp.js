a7a2627c85c28b98ce6f531819bfd06e7b283560
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-26 22:55:36 +0000

    Update xp.js

--- commands/xp.js
+++ commands/xp.js
@@ -1,25 +1,12 @@
 const Discord = module.require("discord.js");
 const client = new Discord.Client();
-const mentionHook = new Discord.WebhookClient("526376828290596865", "xR-_drIyJJKyEv11nH8pUgfkm5EndIY3wdMD77lVEgpMW2nRc9qofei23BkqEriF6gOy");
 
 module.exports.run = async (client, message, args) => {
-	//!addrole @andrew Dog Person
-  if (message.author.id !== '501649887411175435') return message.reply("Sorry pal, you can't do that.");
-  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember) return message.reply("Couldn't find that user, yo.");
-  let role = args[1];
-  if(!role) return message.reply("Specify a role!");
-  let gRole = message.guild.roles.find(`id`, role);
-  if(!gRole) return message.reply("Couldn't find that role.");
-
-  if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
-  await(rMember.addRole(gRole.id));
-
-  try{
-    await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
-  }catch(e){
-    message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
-  }
+	con.query(`SELECT * FROM xp WHERE id = '${message.author.id}'`, (err,rows) => {
+		if(err) throw err;
+		let xp = rows[0].xp;
+		message.channel.send(xp);
+	});
 }
 
 module.exports.help = {
